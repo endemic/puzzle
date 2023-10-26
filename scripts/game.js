0cc2ad379aeaf19e1411fbe6a882e8b45bd0d04a
endemic <n@demick.org>
2023-10-25 22:01:43 -0400

    Update grid lib

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,4 @@
-const EMPTY = 16;
+const EMPTY = 'sixteen';
 
 class Game extends Grid {
   constructor() {
@@ -7,28 +7,6 @@ class Game extends Grid {
 
     super(rows, columns);
 
-    // our grid contains simple integers to represent game objects;
-    // this map translates the numbers to a string, that can then be used as
-    // human-readable reference or CSS class (for display purposes)
-    this.cssClassMap = {
-        1: 'one',
-        2: 'two',
-        3: 'three',
-        4: 'four',
-        5: 'five',
-        6: 'six',
-        7: 'seven',
-        8: 'eight',
-        9: 'nine',
-        10: 'ten',
-        11: 'eleven',
-        12: 'twelve',
-        13: 'thirteen',
-        14: 'fourteen',
-        15: 'fifteen',
-        16: 'sixteen'
-    };
-
     // bind context variable to the current Game() object
     // for each of these global handlers/interval
     const grid = document.querySelector('#grid');
@@ -36,13 +14,13 @@ class Game extends Grid {
     grid.addEventListener('click', this.onClick.bind(this));
 
     // randomly populate the grid
-    let nextState = this.displayStateCopy();
-    let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let nextState = this.currentState;
+    let tiles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen'];
 
     for (let y = 0; y < this.rows; y += 1) {
       for (let x = 0; x < this.columns; x += 1) {
-        const randomIndex = Math.floor(Math.random() * values.length);
-        nextState[x][y] = values.splice(randomIndex, 1)[0];
+        const randomIndex = Math.floor(Math.random() * tiles.length);
+        nextState[x][y] = tiles.splice(randomIndex, 1)[0];
       }
     }
 
@@ -60,7 +38,7 @@ class Game extends Grid {
         y: parseInt(event.target.dataset.y, 10)
     };
 
-    let state = this.displayStateCopy();
+    let nextState = this.currentState;
     let neighbors = this.getNeighbors(clicked.x, clicked.y);
 
     // check each neighboring cell
@@ -68,10 +46,10 @@ class Game extends Grid {
       let point = neighbors[i];
 
       // if one is empty...
-      if (state[point.x][point.y] === EMPTY) {
+      if (nextState[point.x][point.y] === EMPTY) {
         // swap it with the one that was clicked
-        state[point.x][point.y] = state[clicked.x][clicked.y];
-        state[clicked.x][clicked.y] = EMPTY;
+        nextState[point.x][point.y] = nextState[clicked.x][clicked.y];
+        nextState[clicked.x][clicked.y] = EMPTY;
 
         sona.play('click');
 
@@ -79,15 +57,12 @@ class Game extends Grid {
       }
     }
 
-    this.render(state);
+    this.render(nextState);
 
     this.checkWinCondition();
   }
 
   getNeighbors(x, y) {
-    // function to ensure that (x, y) coords are within our data structure
-    const withinBounds = ({ x, y }) => x >= 0 && x < this.columns && y >= 0 && y < this.rows;
-
     return [
         // previous row
         { x: x, y: y - 1 },
@@ -96,37 +71,33 @@ class Game extends Grid {
         { x: x + 1, y: y },
         // next row
         { x: x, y: y + 1 },
-    ].filter(withinBounds);
+    ].filter(point => this.withinBounds(point));
   }
 
   checkWinCondition() {
-    let won = true;
-    let expected = 1;
-    let state = this.displayStateCopy();
+    let index = 0;
+    let state = this.currentState;
+    let tiles = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen'];
 
-    // basically go through the rows/columns and expect the
-    // value of each cell to be sequential, 1 -> 16
+    // iterate through the rows/columns and expect the
+    // value of each cell to match the ordered `tiles` list
+    // nest the x loop inside the y loop, so the grid can be traversed like this:
+    // [ 1][ 2][ 3][ 4]
+    // [ 5][ 6][ 7][ 8]
+    // [ 9][10][11][12]
+    // [13][14][15][16]
     for (let y = 0; y < this.rows; y += 1) {
-      if (!won) {
-        break;
-      }
-
       for (let x = 0; x < this.columns; x += 1) {
-        if (state[x][y] === expected) {
-          expected += 1;
-        } else {
-          won = false;
-          break;
+        if (state[x][y] !== tiles[index]) {
+          console.log(`${tiles[index]} is not in the right position`);
+          return false;
         }
+
+        index += 1;
       }
     }
 
-    if (won) {
-      sona.play('tada');
-
-      this.gameOver = true;
-    } else {
-      console.log(`${expected} is not in the right position`);
-    }
+    sona.play('tada');
+    this.gameOver = true;
   }
 }
